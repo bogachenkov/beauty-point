aae0caabe102c57d93ce2a92f677bc68867ffbf8
Nikita Bogachenkov <deskotno@gmail.com>
2019-08-20 15:36:39 +0300

    Updates

---
+++ precache-manifest.4cc638a340291bfe68ba665dc7107e75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd1191ccabf7f7d10176f742339acb8",
+    "url": "/beauty-point/index.html"
+  },
+  {
+    "revision": "c74d8c238d48436f3994",
+    "url": "/beauty-point/static/css/main.f819b7a5.chunk.css"
+  },
+  {
+    "revision": "262feb59bdafab8e4ad8",
+    "url": "/beauty-point/static/js/2.117ee525.chunk.js"
+  },
+  {
+    "revision": "c74d8c238d48436f3994",
+    "url": "/beauty-point/static/js/main.90a1efc5.chunk.js"
+  },
+  {
+    "revision": "20978e5a558fe6d47f9c",
+    "url": "/beauty-point/static/js/runtime~main.c856e9b3.js"
+  }
+]);
